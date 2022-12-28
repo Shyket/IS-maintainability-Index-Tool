9c13ddb95bbedf1ad6914f99c4f5c9f756c5e6eb
munimul islam <munimulislam@iut-dhaka.edu>
2022-12-28 22:34:10 +0600

    earth js test file added

---
+++ earth.js
@@ -0,0 +1,43 @@
+let p, q;
+const nani = "v";
+var i = 8,
+  j = 90;
+const ow = () => {
+  let y = j + i;
+};
+if (i < j) {
+  console.log("og");
+  while (i < 6) {
+    i--;
+    //catch me
+    if (i == 7) {
+      for (p = 0; p < 6; p++) {
+        if (p == 4) {
+          p++;
+          i--;
+        } else {
+          p--;
+          i++;
+        }
+      }
+    }
+  }
+  i = 800;
+} else if (i > j) {
+} else if (i == 900) {
+  console.log("pichi");
+} else if (i == 900) {
+  console.log("huka");
+} else {
+  console.log("cute");
+}
+
+function name(params) {
+  let fff = params;
+}
+// function(): void{
+//     var k = null;
+// }
+// const ok = () => {
+//     var f = "hh";
+// }
